835db4dc744bd682717c05ef9c37fe3a68f59d63
Ram kumar <webbieram@outlook.com>
2022-07-08 00:35:31 +0530

    assertions added

--- src/components/TodoFooter/TodoFooter.js
+++ src/components/TodoFooter/TodoFooter.js
@@ -6,11 +6,19 @@ function TodoFooter({
     numberOfIncompleteTasks
 }) {
     return (
-        <div className="todo-footer">
-            <p>{numberOfIncompleteTasks} {numberOfIncompleteTasks === 1 ? "task" : "tasks"} left</p>
-            <Link to="/followers">Followers</Link>
-        </div>
-    )
+      <div className="todo-footer">
+        {/* The Test fails due to the visibility is none */}
+        {/* <p style={{opacity: 0}}>
+          {numberOfIncompleteTasks}{' '}
+          {numberOfIncompleteTasks === 1 ? 'task' : 'tasks'} left
+        </p> */}
+        <p data-testid='para'>
+          {numberOfIncompleteTasks}{' '}
+          {numberOfIncompleteTasks === 1 ? 'task' : 'tasks'} left
+        </p>
+        <Link to="/followers">Followers</Link>
+      </div>
+    );
 }
 
 export default TodoFooter
